462209267ca9906aac396e780bd2edc3a94afb1b
Alexander Lazarenko <ferrosful@gmail.com>
2023-02-05 17:09:25 +0200

    Added all necessary types for the home page.

---
+++ typing.d.ts
@@ -0,0 +1,18 @@
+type ProductRating = {
+  rate: number;
+  count: number;
+};
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description?: string;
+  category: string;
+  image?: string;
+  rating?: ProductRating;
+};
+
+export interface HomePageProps {
+  products: Product[];
+}
