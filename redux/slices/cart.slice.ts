a8274d75965a3dc00602051c64a1e3223db1235e
Alexander Lazarenko <ferrosful@gmail.com>
2023-02-06 16:31:30 +0200

    Implemented cart state management using RTK.

---
+++ redux/slices/cart.slice.ts
@@ -0,0 +1,48 @@
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from 'redux/store';
+import { Product } from 'typing';
+
+interface CartProduct extends Product {
+  quantity: number;
+}
+
+type CartState = {
+  products: CartProduct[];
+};
+
+const initialState: CartState = {
+  products: [],
+};
+
+const { actions, reducer } = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addProduct: (state, { payload }: PayloadAction<Product>) => {
+      const product = state.products.find(({ id }) => id === payload.id);
+      if (!product) {
+        state.products = [...state.products, { ...payload, quantity: 1 }];
+      } else {
+        state.products = state.products.map((prod) =>
+          prod.id === payload.id ? { ...prod, quantity: prod.quantity + 1 } : prod,
+        );
+      }
+    },
+    removeProduct: (state, { payload }: PayloadAction<Product>) => {
+      state.products = state.products.filter(({ id }) => id !== payload.id);
+    },
+  },
+});
+
+export const { addProduct, removeProduct } = actions;
+
+export const selectProducts = createSelector(
+  (state: RootState) => state.cart.products,
+  (products) => products,
+);
+export const selectQuantity = createSelector(
+  (state: RootState, productId: number) => state.cart.products.find(({ id }) => id === productId),
+  (product) => product?.quantity ?? 0,
+);
+
+export default reducer;
